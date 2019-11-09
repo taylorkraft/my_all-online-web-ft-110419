f635b459394974f07cad9e372d6685896e441ab1
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-09 01:50:27 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,17 @@ require 'pry'
 
 def my_all?(collection)
   x = 0
+  block_return_values = []
  while x < collection.length
- yield(collection[x])
+ block_return_values << yield(collection[x])
   x += 1 
+ end
+ 
+ if
+   block_return_values.include?(false)
+   false
+ else
+   true 
  end
 end
 
